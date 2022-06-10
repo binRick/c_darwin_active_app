76889631f78e7320666cb7421214b43dcd7b5c52
rick <rick@ricks-MacBook-Air.local>
2022-06-09 21:16:29 -0400

    base meson build requirements

---
+++ active-app-test/active-app-test.c
@@ -0,0 +1,8 @@
+#include "active-app-test.h"
+
+
+int main(int argc, char **argv) {
+  (void)argc; (void)argv;
+  printf("ok\n");
+  return(0);
+}
---
+++ active-app/active-app.c
@@ -0,0 +1,8 @@
+#pragma once
+#include "active-app.h"
+
+
+int main(const int argc, const char **argv){
+  printf("OK\n");
+}
+
